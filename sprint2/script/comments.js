28609742de1ce2195c8d4c7cf323e3cc09eef76a
Brian Alves <brianalves801@gmail.com>
2020-02-07 14:51:32 -0800

    fixed comment alignment and finished axios

--- sprint2/script/comments.js
+++ sprint2/script/comments.js
@@ -1,45 +1,68 @@
-const commentContainer = document.getElementById("comment__container")
+const commentContainer = document.getElementById("comment__container");
 
-let comment1 = { Name: "Michael Lyons", Comment: "They BLEW the ROOF off at their last show, once everyone strted figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed." }
-let comment2 = { Name: "Gary Wong", Comment: "Every time I see hiim shred I feel so motivated to get off my couch and hop onn my board. He's so talented! I wish I can ride like him one day so I can really enjoy myself!" }
-let comment3 = { Name: "Theodore Duncan", Comment: "How can someone be so good!!! You can tell her lives for this and loves to do it every day. Everytime I see him i feel instantly happy! He's definitely my favorite ever!" }
+let comment1 = {
+  Name: "Michael Lyons",
+  Comment:
+    "They BLEW the ROOF off at their last show, once everyone strted figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed."
+};
+let comment2 = {
+  Name: "Gary Wong",
+  Comment:
+    "Every time I see hiim shred I feel so motivated to get off my couch and hop onn my board. He's so talented! I wish I can ride like him one day so I can really enjoy myself!"
+};
+let comment3 = {
+  Name: "Theodore Duncan",
+  Comment:
+    "How can someone be so good!!! You can tell her lives for this and loves to do it every day. Everytime I see him i feel instantly happy! He's definitely my favorite ever!"
+};
 
-let comments = [comment1, comment2, comment3]
-// console.log(document.getElementById("--comment"))
-document.getElementById("--button").addEventListener("click", function() { createComment(document.getElementById("--name").value,document.getElementById("--comment").value)});
-
-
-
+let comments = [comment1, comment2, comment3];
+document.getElementById("--button").addEventListener("click", function() {
+  console.log("form Submitted!");
+  let data = {
+    name: document.getElementById("--name").value,
+    comment: document.getElementById("--comment").value
+  };
+  axios
+    .post(
+      "https://project-1-api.herokuapp.com/comments?api_key=f0aeed98-0800-4b90-95f0-abfd8e5e7234",
+      data
+    )
+    .then(response => {});
+  createComment(
+    document.getElementById("--name").value,
+    document.getElementById("--comment").value
+  );
+});
 
 function createComment(name, comment) {
-    let commentContainerTwo = document.createElement('div')
-    let cc = document.getElementById("comment__container")
-    let commentPic = document.createElement('div')
-    commentPic.classList.add("comment__pic")
-    commentContainerTwo.appendChild(commentPic)
-    let commentName = document.createElement('h3')
-    // commentName.innerHTML = 'Name'
-    commentContainerTwo.appendChild(commentName);
-    let commentsNameComment = document.createElement('p')
-    commentsNameComment.innerHTML = name;
-    commentContainerTwo.appendChild(commentsNameComment);
-   
-    let commentsComment = document.createElement('h3')
-    // commentsComment.innerHTML = 'Comment'
-    commentContainerTwo.appendChild(commentsComment)
-    let commentThree = document.createElement('p')
-    commentThree.innerHTML = comment;
-    commentContainerTwo.appendChild(commentThree)
+  let commentContainerTwo = document.createElement("div");
+  let cc = document.getElementById("comment__container");
+  let commentPic = document.createElement("div");
+  commentPic.classList.add("comment__pic");
+  commentContainerTwo.appendChild(commentPic);
+  let commentName = document.createElement("h3");
+  commentContainerTwo.appendChild(commentName);
+  let commentsNameComment = document.createElement("p");
+  commentsNameComment.innerHTML = name;
+  commentContainerTwo.appendChild(commentsNameComment);
 
-    cc.insertBefore(commentContainerTwo, cc.childNodes[0]);  
-    console.log(cc.childNodes)
-        
+  let commentsComment = document.createElement("h3");
+  commentContainerTwo.appendChild(commentsComment);
+  let commentThree = document.createElement("p");
+  commentThree.innerHTML = comment;
+  commentContainerTwo.appendChild(commentThree);
+
+  cc.insertBefore(commentContainerTwo, cc.childNodes[0]);
 }
 
-for (i = 0; i < comments.length; i++) {
-    createComment(comments[i].Name, comments[i].Comment)
-
-
-    // console.log(name);
-    // console.log(comment);
-}
+axios
+  .get(
+    "https://project-1-api.herokuapp.com/comments?api_key=f0aeed98-0800-4b90-95f0-abfd8e5e7234"
+  )
+  .then(response => {
+    console.log(response.data);
+    let loadComments = response.data;
+    for (i = 0; i < loadComments.length; i++)
+      createComment(loadComments[i].name, loadComments[i].comment);
+  });
--- sprint2/script/shows.js
+++ sprint2/script/shows.js
@@ -1,77 +1,87 @@
-const showContainer = document.getElementById("show__container")
+const showContainer = document.getElementById("show__container");
 
-let show1 = { Date: "Mon, Dec 17, 2018", Venue: "Ronald Lane", Location: "San Francisco, CA" }
-let show2 = { Date: "Tue, Jul 18, 2019", Venue: "Pier 3 East", Location: "San Francisco, CA" }
-let show3 = { Date: "Fri, Jul 22, 201p", Venue: "View Loungue", Location: "San Francisco, CA" }
-let show4 = { Date: "Fri, Sep 05, 2019", Venue: "Moscow Center", Location: "San Francisco, CA" }
-let show5 = { Date: "Sat, Aug 12, 2019", Venue: "Hyatt Agency", Location: "San Francisco, CA" }
-let show6 = { Date: "Wed, Aug 11, 2019", Venue: "Pres Club", Location: "San Francisco, CA" }
+let show1 = {
+  Date: "Mon, Dec 17, 2018",
+  Venue: "Ronald Lane",
+  Location: "San Francisco, CA"
+};
+let show2 = {
+  Date: "Tue, Jul 18, 2019",
+  Venue: "Pier 3 East",
+  Location: "San Francisco, CA"
+};
+let show3 = {
+  Date: "Fri, Jul 22, 201p",
+  Venue: "View Loungue",
+  Location: "San Francisco, CA"
+};
+let show4 = {
+  Date: "Fri, Sep 05, 2019",
+  Venue: "Moscow Center",
+  Location: "San Francisco, CA"
+};
+let show5 = {
+  Date: "Sat, Aug 12, 2019",
+  Venue: "Hyatt Agency",
+  Location: "San Francisco, CA"
+};
+let show6 = {
+  Date: "Wed, Aug 11, 2019",
+  Venue: "Pres Club",
+  Location: "San Francisco, CA"
+};
 
-let shows = [show1, show2, show3, show4, show5, show6]
+let shows = [show1, show2, show3, show4, show5, show6];
 
-// function createShows ()
-// let i;
-// for (i = 0; i < shows.length; i++) 
+function createShows(showinfo) {
+  let showsDiv = document.createElement("div");
+  showsDiv.classList.add("shows__div");
 
-function createShows (showinfo){
-    let showsDiv = document.createElement('div')
-    showsDiv.classList.add('shows__div')
+  let showsDate = document.createElement("h2");
 
-    let showsDate = document.createElement('h2')
+  showsDate.innerHTML = "Date";
+  showsDiv.appendChild(showsDate);
+  let showsDateContext = document.createElement("p");
+  showsDateContext.innerHTML = showinfo.date;
+  showsDiv.appendChild(showsDateContext);
+  showsDate.classList.add("shows__header");
+  showsDateContext.classList.add("shows__date");
 
-    showsDate.innerHTML = 'Date'
-    showsDiv.appendChild(showsDate);
-    let showsDateContext = document.createElement('p')
-    showsDateContext.innerHTML = shows[i].Date;
-    showsDiv.appendChild(showsDateContext);
-    showsDate.classList.add('shows__header')
-    showsDateContext.classList.add('shows__date')
+  let showsVenue = document.createElement("h2");
+  showsVenue.innerHTML = "Venue";
+  showsDiv.appendChild(showsVenue);
+  let showsVenueContext = document.createElement("p");
+  showsVenueContext.innerHTML = showinfo.place;
+  showsDiv.appendChild(showsVenueContext);
+  showsVenue.classList.add("shows__header");
 
-    let showsVenue = document.createElement('h2')
-    showsVenue.innerHTML = 'Venue'
-    showsDiv.appendChild(showsVenue);
-    let showsVenueContext = document.createElement('p')
-    showsVenueContext.innerHTML = shows[i].Venue;
-    showsDiv.appendChild(showsVenueContext);
-    showsVenue.classList.add('shows__header')
+  let showsLocation = document.createElement("h2");
+  showsLocation.innerHTML = "Location";
+  showsDiv.appendChild(showsLocation);
+  let showsLocationContext = document.createElement("p");
+  showsLocationContext.innerHTML = showinfo.location;
+  showsDiv.appendChild(showsLocationContext);
+  showsLocation.classList.add("shows__header");
 
-    let showsLocation = document.createElement('h2')
-    showsLocation.innerHTML = 'Location'
-    showsDiv.appendChild(showsLocation)
-    let showsLocationContext = document.createElement('p')
-    showsLocationContext.innerHTML = shows[i].Location;
-    showsDiv.appendChild(showsLocationContext);
-    showsLocation.classList.add('shows__header')
+  let showsButton = document.createElement("button");
+  showsButton.innerHTML = "BUY TICKETS";
+  showsDiv.appendChild(showsButton);
+  showsButton.classList.add("shows__button");
+  let showsButtonHr = document.createElement("hr");
+  showsDiv.appendChild(showsButtonHr);
+  showsButtonHr.classList.add("shows__buttonhr");
 
-    let showsButton = document.createElement("button")
-    showsButton.innerHTML = 'BUY TICKETS'
-    showsDiv.appendChild(showsButton);
-    showsButton.classList.add('shows__button')
-    let showsButtonHr = document.createElement("hr")
-    showsDiv.appendChild(showsButtonHr);
-    showsButtonHr.classList.add('shows__buttonhr')
-
-    showContainer.appendChild(showsDiv);
+  showContainer.appendChild(showsDiv);
 }
 
-
 var showsDateArray = [];
-const showsDate= []
-axios.get("https://project-1-api.herokuapp.com/showdates?api_key=ebe808ae-e0c8-4242-9247-b5a4cb1ea502") 
-    .then(response =>  {
-        console.log(response.data);
-        let loadShows= response.data;
-        for (i = 0; i < loadShows.length; i++)
-        // try calling a function and passing the response.data to the new function\
-        // the function could use the code you have above except use response.data to loop over
-    
-    createShows(loadShows[i]) })
-
-
-// function loadShows(showData){z
-//     console.log({showData});
-//     // move your code that's above which is appending elements into this function block
-// }
-
-
-
+const showsDate = [];
+axios
+  .get(
+    "https://project-1-api.herokuapp.com/showdates?api_key=f0aeed98-0800-4b90-95f0-abfd8e5e7234"
+  )
+  .then(response => {
+    console.log(response.data);
+    let loadShows = response.data;
+    for (i = 0; i < loadShows.length; i++) createShows(loadShows[i]);
+  });
